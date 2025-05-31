a0582bc76d6bc3a1ffdb29cb03991f06613f914f
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-05-31 18:06:31 -0300

    feat: add webhook processing and user services with corresponding routes and models

---
+++ providers/app_provider.ts
@@ -0,0 +1,32 @@
+import type { ApplicationService } from '@adonisjs/core/types'
+
+export default class AppProvider {
+  constructor(protected app: ApplicationService) {}
+
+  /**
+   * Register bindings to the container
+   */
+  register() {}
+
+  /**
+   * The container bindings have booted
+   */
+  async boot() {
+    await import('#extensions/logged-user.extension')
+  }
+
+  /**
+   * The application has been booted
+   */
+  async start() {}
+
+  /**
+   * The process has been started
+   */
+  async ready() {}
+
+  /**
+   * Preparing to shut down the app
+   */
+  async shutdown() {}
+}

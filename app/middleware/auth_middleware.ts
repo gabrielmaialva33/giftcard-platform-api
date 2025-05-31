53c126b7f25f79fb4ac7567844d4f4fa5850d3c0
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-05-31 18:06:03 -0300

    feat: update auth middleware to store logged user ID instead of user object

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -1,6 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import type { NextFn } from '@adonisjs/core/types/http'
 import type { Authenticators } from '@adonisjs/auth/types'
+import User from '#modules/user/models/user.js'
 
 /**
  * Auth middleware is used to authenticate HTTP requests and deny
@@ -8,7 +9,7 @@ import type { Authenticators } from '@adonisjs/auth/types'
  */
 export default class AuthMiddleware {
   /**
-   * The URL to redirect to, when authentication fails
+   * The URL to redirect to when authentication fails
    */
   redirectTo = '/login'
 
@@ -20,7 +21,7 @@ export default class AuthMiddleware {
     } = {}
   ) {
     await ctx.auth.authenticateUsing(options.guards, { loginRoute: this.redirectTo })
-    ctx['loggedUser'] = ctx.auth.user as any
+    ctx['loggedUserId'] = ctx.auth.user!.id as unknown as User['id']
     return next()
   }
 }

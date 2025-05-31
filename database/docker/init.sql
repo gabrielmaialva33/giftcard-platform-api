ed3453426a9ef5a3f491e6d06604cf4485a57550
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-05-31 15:38:38 -0300

    chore: update init.sql for improved clarity in comments

--- database/docker/init.sql
+++ database/docker/init.sql
@@ -1,8 +1,8 @@
--- Criar extensões necessárias
+-- Create necessary extensions
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
 
--- Configurar TimescaleDB
+-- Configure TimescaleDB
 SELECT timescaledb_pre_restore();
 SELECT timescaledb_post_restore();

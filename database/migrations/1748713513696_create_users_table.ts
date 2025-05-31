233fe670243596d0adeeb75ab11bdef92217bb10
Gabriel Maia <gabrielmaialva33@gmail.com>
2025-05-31 15:30:24 -0300

    chore: update users table schema to include phone, role, and status fields

--- database/migrations/1748713513696_create_users_table.ts
+++ database/migrations/1748713513696_create_users_table.ts
@@ -6,9 +6,17 @@ export default class extends BaseSchema {
   async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').notNullable()
+
       table.string('full_name').nullable()
       table.string('email', 254).notNullable().unique()
       table.string('password').notNullable()
+
+      table.string('phone', 20).nullable()
+      table
+        .enum('role', ['franchisor', 'franchisee', 'establishment'])
+        .notNullable()
+        .defaultTo('establishment')
+      table.enum('status', ['active', 'inactive', 'suspended']).notNullable().defaultTo('active')
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
